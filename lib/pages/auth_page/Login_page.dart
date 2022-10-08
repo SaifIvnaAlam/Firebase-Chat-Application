28b1efa93db410f9181efa0e2193a9d002e4a2b7
Saif ivna alam <73099787+SaifIvnaAlam@users.noreply.github.com>
2022-10-08 23:35:08 +0600

    added: log in form validators

--- lib/pages/auth_page/Login_page.dart
+++ lib/pages/auth_page/Login_page.dart
@@ -8,10 +8,71 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String email = '';
+  String password = '';
+  final formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Log in Page")),
-    );
+    return Scaffold(
+        body: Center(
+      child: Form(
+        key: formkey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(hintText: "Email"),
+              onChanged: (value) {
+                setState(
+                  () {
+                    email = value;
+                    print(email);
+                  },
+                );
+              },
+              validator: (value) {
+                return RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                        .hasMatch(value!)
+                    ? null
+                    : "Please Enter a valid Email";
+              },
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(hintText: "Password"),
+              onChanged: (value) {
+                setState(
+                  () {
+                    password = value;
+                    print(password);
+                  },
+                );
+              },
+              validator: (value) {
+                if (value!.length < 6) {
+                  return "Password Must be at least 6";
+                } else {
+                  return null;
+                }
+              },
+            ),
+            MaterialButton(
+              color: Colors.green,
+              onPressed: () {},
+              child: const Text("Log in"),
+            ),
+            MaterialButton(
+              color: Colors.blue,
+              onPressed: () {},
+              child: const Text("register now"),
+            )
+          ],
+        ),
+      ),
+    ));
   }
+
+  logIn() {}
 }
