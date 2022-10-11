83d88956ffd0b5deda33541e933b0ca8a3339461
Saif ivna alam <73099787+SaifIvnaAlam@users.noreply.github.com>
2022-10-11 21:21:39 +0600

    added: Home screen, log out

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,8 @@
+import 'package:chat_app/helper/helper_fancttion.dart';
+import 'package:chat_app/helper/widgets.dart';
+import 'package:chat_app/pages/auth_page/Login_page.dart';
+
+import 'package:chat_app/pages/search_page.dart';
 import 'package:chat_app/service/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -10,16 +15,73 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   AuthService authService = AuthService();
+  String? username = "";
+  String? email = "";
+
+  @override
+  void initState() {
+    getingUserData();
+    super.initState();
+  }
+
+  void getingUserData() async {
+    await HelperFunction.getUserNameFromSF().then((value) {
+      setState(() {
+        username = value;
+      });
+    });
+    await HelperFunction.getUserEmailSF().then((value) {
+      setState(() {
+        email = value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: MaterialButton(
-        onPressed: () {
-          authService.signOut();
-        },
-        child: const Text(" Sign out"),
-      )),
+      appBar: AppBar(
+        title: const Text("Groups"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                nextScreen(context, const SearchPage());
+              },
+              icon: const Icon(Icons.search))
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const Icon(
+              Icons.account_circle_outlined,
+              size: 80,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Center(
+              child: Text(
+                username!,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            MaterialButton(
+              onPressed: () {
+                authService.signOut();
+                nextScreen(context, LoginPage());
+              },
+              child: Row(
+                children: [Icon(Icons.logout), Text("Log out")],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
